58cd3c3fb9ae6467b7c0d7015560a730d5904af0
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-10 18:28:42 +0200

    ciclo for completo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-// Stampiamo i numeri da 1 a 100
+// stampo i numeri da 1 a 100
 for (let x = 1; x <= 100; x++) {
-    if (x%3 === 0)
+    // controllo i multipli di entrambi
+    if (x % 3 === 0 && x % 5 === 0)
+        console.log("fizzBuzz")
+// controllo multipli di 3
+    else if (x % 3 === 0)
         console.log("buzz");
+// controllo multipli di 5
+    else if (x % 5 === 0)
+    console.log("fizz");
+    
+    else(console.log(x))
 }
 
+const elementoH1 = document.getElementById("#elemento")
 
